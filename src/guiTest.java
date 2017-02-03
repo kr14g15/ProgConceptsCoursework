36ef3f55b698155343edb256cbbadcf389f1a58c
szpeplinski <sp5u14@soton.ac.uk>
2017-02-03 13:00:52 +0000

    Test

--- src/guiTest.java
+++ src/guiTest.java
@@ -1,10 +1,24 @@
 import javax.swing.*;
 
 public class guiTest {
+    //region PrivateVariables
     private JPanel jPanelTest;
     private JButton button1;
     private JButton button2;
     private JTextArea textArea1;
+    //endregion
+
+    //region PublicVariables
+    //endregion
+
+    //region PrivateMethods
+    //endregion
+
+    //region PublicMethods
+    //endregion
+
+    //region Events
+    //endregion
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Test");
