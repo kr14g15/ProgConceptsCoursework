d9a3663b653c4d0c0ab975ed6132720e655e978f
szymon <sp5u14@soton.ac.uk>
2017-02-17 00:22:19 +0000

    Introduction to TopView Visualization

---
+++ src/Runway.java
@@ -0,0 +1,94 @@
+public class Runway {
+    //region PrivateVariables
+    private String sName;
+    private int intDegree;
+    private int intTORA;
+    private int intTODA;
+    private int intASDA;
+    private int intLDA;
+
+    private int intThreshold;
+    private int intClearway;
+    private int intStopway;
+    private int intRESA;
+    private int intStripEnd;
+    private int intBlastProtection;
+    private int intALS;
+    private int intTOCS;
+    private int intRunwayStrip;
+    //endregion
+
+    //region PublicVariables
+    //endregion
+
+    //region PrivateMethods
+    //endregion
+
+    //region PublicMethods
+    //endregion
+
+    //region Events
+    //endregion
+
+    //region Getters
+    public String getsName() {
+        return sName;
+    }
+
+    public int getIntDegree() {
+        return intDegree;
+    }
+
+    public int getIntTORA() {
+        return intTORA;
+    }
+
+    public int getIntTODA() {
+        return intTODA;
+    }
+
+    public int getIntASDA() {
+        return intASDA;
+    }
+
+    public int getIntLDA() {
+        return intLDA;
+    }
+
+    public int getIntThreshold() {
+        return intThreshold;
+    }
+
+    public int getIntClearway() {
+        return intClearway;
+    }
+
+    public int getIntStopway() {
+        return intStopway;
+    }
+
+    public int getIntRESA() {
+        return intRESA;
+    }
+
+    public int getIntStripEnd() {
+        return intStripEnd;
+    }
+
+    public int getIntBlastProtection() {
+        return intBlastProtection;
+    }
+
+    public int getIntALS() {
+        return intALS;
+    }
+
+    public int getIntTOCS() {
+        return intTOCS;
+    }
+
+    public int getIntRunwayStrip() {
+        return intRunwayStrip;
+    }
+    //endregion
+}
---
+++ src/TopViewRunway2DVisualization.java
@@ -0,0 +1,20 @@
+public class TopViewRunway2DVisualization {
+    //region PrivateVariables
+    private Runway runway;
+    //endregion
+
+    //region PublicVariables
+    //endregion
+
+    //region PrivateMethods
+    //endregion
+
+    //region PublicMethods
+    //endregion
+
+    //region Events
+    //endregion
+
+    //region Getters
+    //endregion
+}
