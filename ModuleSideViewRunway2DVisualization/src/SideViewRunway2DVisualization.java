4f9456bf52ad39377c895e1a403cb052b898f6ac
szpeplinski <sp5u14@soton.ac.uk>
2017-02-19 04:44:38 +0000

    GUI update + MODULE update

---
+++ ModuleSideViewRunway2DVisualization/src/SideViewRunway2DVisualization.java
@@ -0,0 +1,5 @@
+import javafx.embed.swing.JFXPanel;
+
+public class SideViewRunway2DVisualization extends JFXPanel {
+
+}
---
+++ ModuleUKMapAirportSelection2DVisualization/src/UKMapAirportSelection2DVisualization.java
@@ -0,0 +1,5 @@
+import javafx.embed.swing.JFXPanel;
+
+public class UKMapAirportSelection2DVisualization extends JFXPanel {
+
+}
