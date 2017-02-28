5b818bfc83b93e1810a1f09e31baab9f3c1bad5e
szpeplinski <sp5u14@soton.ac.uk>
2017-02-28 22:08:34 +0000

    Clearway + stopway update --visualization button --Picture remaining --need clearway, stopway --EDIT txtBoxes --incorrect refresh of the right threshold --refresh when editing runway --comboBox for runwalist needs proper runway displaying --letters on strip might not be in correct order. --Airport image change when editing not finished. --distance between first and last strip not equal --need to draw Instrument Strip --need to draw Arrow showing direction --We can use arrows insead of '|' characters when determining  size --textures insead of coloured rectangles. --needs font resize, --Runway and Airport Dropbox selection not the same

---
+++ src/ColorScheme.java
@@ -0,0 +1,29 @@
+import javafx.scene.paint.Color;
+
+public enum ColorScheme {
+        DEFAULT(Color.rgb(34,139,34), Color.rgb(222,184,135), Color.rgb(178,178,254),
+                Color.rgb(144,238,144),Color.rgb(128,128,128),Color.rgb(255,255,255),Color.rgb(0,0,0)),
+
+        COLOR_BLIND(Color.rgb(36,255,36), Color.rgb(146,73,0), Color.rgb(73,0,146),
+                    Color.rgb(0,146,146),Color.rgb(146,0,0),Color.rgb(255,255,255),Color.rgb(0,0,0));
+
+        public Color BackgroundColor;
+        public Color GroundColor;
+        public Color SkyColor;
+        public Color CAAColor;
+        public Color RunwayStripColor;
+        public Color StripesColor;
+        public Color CharactersColor;
+
+        private ColorScheme(Color backgroundColor, Color groundColor, Color skyColor, Color CAAColor,
+                            Color runwayStripColor,Color stripesColor,Color charactersColor)
+        {
+            this.BackgroundColor = backgroundColor;
+            this.GroundColor = groundColor;
+            this.SkyColor = skyColor;
+            this.CAAColor = CAAColor;
+            this.RunwayStripColor = runwayStripColor;
+            this.StripesColor = stripesColor;
+            this.CharactersColor = charactersColor;
+        }
+}
